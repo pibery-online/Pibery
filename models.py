df7a4e1015db5e5ff8e478adfb20415abc12a0e1
Pibery <info.pibery@gmail.com>
2026-08-11 20:26:57 +0600

    Update models.py

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from flask_login import UserMixin
-from app import db
+from extensions import db
 
 class Category(db.Model):
     id = db.Column(db.Integer, primary_key=True)
